0558f027e4339743744fce47aa44a83da1efe89d
zakarya3 <zakaria.aanni@gmail.com>
2024-11-28 13:06:44 +0100

    refactor: restructure views by removing app.blade.php and adding home.blade.php with layout support

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+        @yield('content')
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
